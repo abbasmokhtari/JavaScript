f2e40f08fa37dafc3ad756a8f41081d5ddc8b571
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2019-05-15 19:30:20 +0100

    Add files via upload
    Initialise a project with JavaScript

---
+++ JS_01.js
@@ -0,0 +1,226 @@
+//alert ('hello World');
+//var userName = prompt ("What is your name?");
+//alert ("Nice to meet you, " + userName);
+// console.log ("Also nice to meet you here, " + userName );
+
+// var userName = prompt ("What is your first name?");
+// var userSirName = prompt ("what is your last name?");
+// var age = prompt ("How old are you?");
+// console.log ("Your full name is " + userName + " " + userSirName);
+// console.log ("You are " + age + " years old");
+
+// var age = prompt ("How old are you?");
+// var day = age * 365;
+// console.log ("You have lived " + day +" days");
+
+// var first = "";
+// var second = "HAHA!";
+// var third = "False!";
+// !((first || second) && third);
+
+// var age = prompt ("How old are you?");
+// if (age<18){
+//     console.log("You can not enter!");
+//     // alert("You can not enter!");
+// }
+//  else if (age <21) {
+//      console.log ("you can enter but can not drink");
+//  }
+
+//  else {
+//      console.log ("You can enter and drink!");
+//  }
+
+// var age = Number(prompt ("How old are you?"));
+// if (age<0) {
+//     console.log ('Wrong input');
+// }
+
+// else if (age == 22) {
+//     console.log ("Happy 22st Birthday");
+// }
+
+// else if ((age % 2 == 1) && (age % Math.sqrt(age) == 0)) {
+//     console.log ("Your age is odd and perfect square");
+// }
+// else if (age % 2 == 1){
+//     console.log("Your age is odd");
+// }
+
+// else if (age % Math.sqrt(age) == 0){
+//     console.log ("Your age is perfect square");
+// }
+
+// var count = 2;
+// while (count < 6) {
+//     console.log ("my number is " + count);
+//     count++;
+// }
+
+// var count = 2;
+// while (count < 19) {
+//     console.log ("my number is " + count);
+//     count+=2;
+// }
+
+// var str = "Hello";
+// var count = 0;
+
+// while (count < str.length) {
+//     console.log (str[count]);
+//     count++;
+// }
+
+// var num = 1;
+// while (num <= 10){
+//    console.log (num);
+//    num+=2;
+// }
+
+// var num = 4;
+// while (num <= 20) {
+//     if (num % 4 === 0){
+//         console.log (num);
+//     }
+//     num++;
+// }
+
+// var num = -10;
+// while (num <= 19){
+//     console.log (num);
+//     num++;
+// }
+
+// for ( var i = -10; i < 20 ; i++){
+// 	console.log(i);
+// }
+
+// var num = 9;
+// while ( num <= 40) {
+//     if (num % 2 === 0){
+//         console.log (num)
+//     };
+//     num++;
+// }
+
+
+// for ( var i = 10; i < 41 ; i += 2){
+//     console.log (i);
+// }
+
+
+// var num = 300;
+// while ( num <= 333) {
+//     if (num % 2 !== 0){
+//         console.log (num)
+//     };
+//     num++;
+// }
+
+// for (var i = 301 ; i < 334; i += 2){
+//     console.log (i)
+// }
+
+// var num = 5;
+// while ( num <= 50) {
+//     if (num % 3 === 0 && num % 5 === 0){
+//         console.log (num)
+//     };
+//     num++;
+// }
+
+// for (var i = 5; i < 51; i++){
+//     if (i % 3 === 0 && i % 5 ===0){
+//         console.log(i);
+//     }
+// }
+
+// var num = 1;
+// while ( num <= 50) {
+//     if (num % 15 === 0) {
+//         console.log ("foooooooooz")
+//     }
+//     else if (num % 3 === 0 ){
+//         console.log ("fiz")
+//     }
+//     else if (num % 5 === 0 ){
+//         console.log ("faz")
+//     }
+//     else {
+//         console.log (num)
+//     };
+//     num++;
+// }
+
+
+
+
+
+// var loc = prompt('Are we there yet?');
+// while (loc !== 'yes' && loc !== "yeah") {
+// 	var loc = prompt('Are we there yet?');
+// }
+// alert('Yaaaay, we are here');
+
+
+
+// var loc = prompt('Are we there yet?');
+// while (loc.indexOf("yes") === -1) {
+// 	var loc = prompt('Are we there yet?');
+// }
+// alert('Yaaaay, we are here');
+
+
+
+// for (var count = 0; count < 6; count++) {
+// 	console.log(count);
+// }
+
+
+
+// var str = "Hello";
+// for (var i = 0; i <  str.length; i++) {
+//     console.log ( str [i] );
+// }
+
+
+// var str = "hello";
+// var i = 0;
+// while (i < str.length ) {
+//     console.log (str[i]);
+//     i++;
+// }
+
+//======================FUNCTIONS============
+
+// function square (num){
+//     console.log (num * num);
+// }
+
+// square(10);
+// square (5);
+
+
+// function sayHello(name){
+//     console.log ("Hello there " + name + "!");
+// }
+
+// sayHello ("MONA");
+
+
+
+// function area (length, width){
+//     console.log (length * width);
+// }
+
+// area (2,5);
+
+
+// function greet (person1, person2, person3){
+//     console.log("Hi " + person1);
+//     console.log("Hi " + person2);
+//     console.log("Hi " + person3);
+// }
+// greet("Abbas", "Mona", "Mahan");
+
+
